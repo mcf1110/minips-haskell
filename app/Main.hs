e4500f38a1ee0f1a67a1d622dd110482fbdb763d
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-02-19 00:41:45 -0300

    initializing registers

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,14 @@ import Lib
 import qualified Data.IntMap.Lazy as IM
 import qualified Data.Word as W
 import Text.Printf (printf)
+import qualified Data.Vector as V
 
 type Memory = IM.IntMap W.Word32
 type Segment = [W.Word32]
+type Registers = V.Vector W.Word32
+
+startingRegisters :: Registers
+startingRegisters = V.replicate 35 0 V.// [(29, 0x7fffeffc), (28, 0x10008000), (32, 0x00400000)]
 
 loadSegment :: Int -> Segment -> Memory
 loadSegment starting = IM.fromAscList . zip addr
@@ -25,4 +30,5 @@ main :: IO ()
 main = do
     textSegment <- readFromFile "./inputs/03.input.text"
     dataSegment <- readFromFile "./inputs/03.input.data"
-    showMemory $ IM.filter (/= 0) $ loadSegment 0x10010000 dataSegment <> loadSegment 0x00400000 textSegment
+    -- showMemory $ IM.filter (/= 0) $ loadSegment 0x10010000 dataSegment <> loadSegment 0x00400000 textSegment
+    print startingRegisters
