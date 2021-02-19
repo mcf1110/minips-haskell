c6b95b29e1365dcc2ccdb6ab89624de6e149fb82
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-02-19 00:22:20 -0300

    loading and showing memory

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,27 @@ module Main where
 
 import Lib
 
+import qualified Data.IntMap.Lazy as IM
+import qualified Data.Word as W
+import Text.Printf (printf)
+
+type Memory = IM.IntMap W.Word32
+type Segment = [W.Word32]
+
+loadSegment :: Int -> Segment -> Memory
+loadSegment starting = IM.fromAscList . zip addr
+    where addr = iterate (+4) starting
+
+showMemory :: Memory -> IO ()
+showMemory m = do
+    putStrLn "\t┌────────────┬────────────┐"
+    putStrLn "\t│    Addr    │    Code    │"
+    putStrLn "\t╞════════════╪════════════╡"
+    mapM_ (\(addr, val) -> putStrLn $ printf "\t│ 0x%08x │ 0x%08x │" addr val) $ IM.assocs m
+    putStrLn "\t└────────────┴────────────┘"
+
 main :: IO ()
-main = someFunc "./inputs/01.soma.text"
+main = do
+    textSegment <- readFromFile "./inputs/03.input.text"
+    dataSegment <- readFromFile "./inputs/03.input.data"
+    showMemory $ IM.filter (/= 0) $ loadSegment 0x10010000 dataSegment <> loadSegment 0x00400000 textSegment
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,20 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (showFile, readFromFile) where
 
 import qualified Data.ByteString as B
+import qualified Data.List.Split as S
+import qualified Data.Word as W
+import Numeric (showHex)
 
-someFunc :: FilePath -> IO ()
-someFunc path = do
+toWord32 :: [W.Word8] -> W.Word32
+toWord32 ws = toEnum $ sum $ zipWith (*) is [2^(x*8) | x <- [3,2..0]]
+  where is = reverse $ fromEnum <$> ws
+
+readFromFile :: FilePath -> IO [W.Word32]
+readFromFile path = do
   contents <- B.readFile path
-  print $ B.unpack contents
+  return $ map toWord32 $ S.chunksOf 4 $ B.unpack contents
+
+showFile :: FilePath -> IO ()
+showFile path = do
+  ws <- readFromFile path
+  mapM_ (putStrLn . (flip showHex "")) ws
