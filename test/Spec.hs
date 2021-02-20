01dcddf4b908fe34f7fd7e93342f8d3f4ede882c
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-02-20 13:39:57 -0300

    teste de putstr desalinhado

--- test/Spec.hs
+++ test/Spec.hs
@@ -164,4 +164,11 @@ runningTests =
          [0x20616c4f, 0x646e756d, 8559]
          [0x3c011001, 0x34240000, 0x24020004, 0xc])
   , testCase "getint" $ assertEqual "" (GetInt) (getSC [] [0x24020005, 0xc])
+  , testCase "putStr desalinhado deve falhar?" $
+    assertEqual
+      ""
+      (PutStr "")
+      (getSC
+         [0, 0, 0x5600203a, 0x20ea636f]
+         [0x3c011001, 0x3424000b, 0x24020004, 0xc])
   ]
