be179989876efddeaf512a9156ca3d7aa0063280
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-03-30 15:58:59 -0300

    changing test names

--- test/RunSpec.hs
+++ test/RunSpec.hs
@@ -43,12 +43,18 @@ runningTests =
 iTests =
   [ testCase "Cannot overwrite $zero" $
     assertEqual "" 0 (regAt 0 $ runSegInitial [0x24000003])
-  , testCase "addi $t0, $zero, 3" $
-    assertEqual "" 3 (regAt 8 $ runSegInitial [0x20080003])
-  , testCase "addi $t1, $zero, 4" $
-    assertEqual "" 4 (regAt 9 $ runSegInitial [0x20090004])
-  , testCase "addiu $sp, $sp, -32" $
-    assertEqual "" 0x7fffefdc (regAt 29 $ runSegInitial [0x27bdffe0])
+  , testGroup
+      "Add Immediate"
+      [ testCase "addi $t0, $zero, 3 - loads 3 to $t0" $
+        assertEqual "" 3 (regAt 8 $ runSegInitial [0x20080003])
+      , testCase "addi $t1, $zero, 4 - loads 4 to $t1" $
+        assertEqual "" 4 (regAt 9 $ runSegInitial [0x20090004])
+      ]
+  , testGroup
+      "Add Immediate Upper"
+      [ testCase "addiu $sp, $sp, -32" $
+        assertEqual "" 0x7fffefdc (regAt 29 $ runSegInitial [0x27bdffe0])
+      ]
   , testCase "add 3 4" $
     assertEqual
       ""
@@ -60,52 +66,64 @@ iTests =
     assertEqual "" 0x10010000 (regAt 4 $ runSegInitial [0x3c011001, 0x34240000])
   , testCase "ori $t1, $t0, 16" $
     assertEqual "" 0x10 (regAt 9 $ runSegInitial [0x24080014, 0x31090010])
-  , testCase "Branch Delay" $
-    assertEqual
-      ""
-      1
-      (regAt 9 $ runSegInitial [0x10000002, 0x24090001, 0x240a0002, 0x240b0003])
-  , testCase "Branch Delay" $
-    assertEqual
-      ""
-      0
-      (regAt 10 $ runSegInitial [0x10000002, 0x24090001, 0x240a0002, 0x240b0003])
-  , testCase "Branch Delay" $
-    assertEqual
-      ""
-      3
-      (regAt 11 $ runSegInitial [0x10000002, 0x24090001, 0x240a0002, 0x240b0003])
-  , testCase "beq $zero, $zero, 1" $
-    assertEqual
-      ""
-      (0x00400000 + (2 * 4))
-      (regAt 32 $ runSegInitial [0x10000001])
-  , testCase "beq $zero, $zero, 1" $
-    assertEqual
-      ""
-      0x0
-      (regAt 4 $ runSegInitial [0x10000002, 0x0, 0x3c011001, 0x34240000])
-  , testCase "bne 1, $zero, 2" $
-    assertEqual
-      ""
-      15
-      (regAt 9 $
-       runSegInitial
-         [0x24090001, 0x15200002, 0x2409000f, 0x240a0002, 0x240b0003])
-  , testCase "bne 1, $zero, 2" $
-    assertEqual
-      ""
-      0
-      (regAt 10 $
-       runSegInitial
-         [0x24090001, 0x15200002, 0x2409000f, 0x240a0002, 0x240b0003])
-  , testCase "bne 1, $zero, 2" $
-    assertEqual
-      ""
-      3
-      (regAt 11 $
-       runSegInitial
-         [0x24090001, 0x15200002, 0x2409000f, 0x240a0002, 0x240b0003])
+  , testGroup
+      "Branch Delay"
+      [ testCase "Branch Delay" $
+        assertEqual
+          "Executes instruction in BDS"
+          1
+          (regAt 9 $
+           runSegInitial [0x10000002, 0x24090001, 0x240a0002, 0x240b0003])
+      , testCase "Does not execute BDS+1" $
+        assertEqual
+          ""
+          0
+          (regAt 10 $
+           runSegInitial [0x10000002, 0x24090001, 0x240a0002, 0x240b0003])
+      , testCase "Branches accordingly" $
+        assertEqual
+          ""
+          3
+          (regAt 11 $
+           runSegInitial [0x10000002, 0x24090001, 0x240a0002, 0x240b0003])
+      ]
+  , testGroup
+      "Branch on Equal"
+      [ testCase "Changes PC correctly" $
+        assertEqual
+          ""
+          (0x00400000 + (2 * 4))
+          (regAt 32 $ runSegInitial [0x10000001])
+      , testCase "Executes correct instructions" $
+        assertEqual
+          ""
+          0x0
+          (regAt 4 $ runSegInitial [0x10000002, 0x0, 0x3c011001, 0x34240000])
+      ]
+  , testGroup
+      "Branch Not Equal - bne 1, $zero, 2"
+      [ testCase "BDS" $
+        assertEqual
+          ""
+          15
+          (regAt 9 $
+           runSegInitial
+             [0x24090001, 0x15200002, 0x2409000f, 0x240a0002, 0x240b0003])
+      , testCase "BDS+1" $
+        assertEqual
+          ""
+          0
+          (regAt 10 $
+           runSegInitial
+             [0x24090001, 0x15200002, 0x2409000f, 0x240a0002, 0x240b0003])
+      , testCase "Branches accordingly" $
+        assertEqual
+          ""
+          3
+          (regAt 11 $
+           runSegInitial
+             [0x24090001, 0x15200002, 0x2409000f, 0x240a0002, 0x240b0003])
+      ]
   , testCase "lw $t0, ans" $
     assertEqual "" 0x2a (regAt 8 $ runSeg [0x2a] [0x3c011001, 0x8c280000])
   , testCase "lw $t1, 4($t0)" $
