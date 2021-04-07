6fcdcdaed34c5f64301ac83ee938dae1a924e48c
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-04-07 17:49:42 -0300

    moving floats and doubles

--- src/Lib/Operation/TypeFR.hs
+++ src/Lib/Operation/TypeFR.hs
@@ -1,10 +1,17 @@
 module Lib.Operation.TypeFR where
 
-import           Control.Monad.State.Lazy
+import           Control.Monad.State.Lazy (get)
+import           Lib.Decode               (FFmt (Double, Single))
 import           Lib.Operation.Helpers    (modifyReg)
-import           Lib.Operation.Infixes
+import           Lib.Operation.Infixes    (($.=))
 import           Lib.Operation.Types
 import qualified Lib.Registers            as R
 
 mfc1 :: RegNum -> RegNum -> Operation ()
 mfc1 rt fs = modifyReg (\r -> R.set rt (R.getCop fs r) r)
+
+mov :: FFmt -> RegNum -> RegNum -> Operation ()
+mov Single fs fd = modifyReg (\r -> R.setCop fd (R.getCop fs r) r)
+mov Double fs fd = do
+  mov Single fs fd
+  mov Single (fs + 1) (fd + 1)
--- test/RunSpec/FRRunSpec.hs
+++ test/RunSpec/FRRunSpec.hs
@@ -3,7 +3,8 @@ module RunSpec.FRRunSpec
   ) where
 
 import           Lib.Computer     (Computer)
-import           RunSpec.Helpers  (floatAt, regAt, runSeg, runSegInitial)
+import           RunSpec.Helpers  (doubleAt, floatAt, regAt, runSeg,
+                                   runSegInitial)
 import           Test.Tasty       (TestTree, testGroup)
 import           Test.Tasty.HUnit (assertEqual, testCase)
 
@@ -16,5 +17,22 @@ frTests =
           ""
           0x40490fdb
           (regAt 8 $ runSeg [0x40490fdb] [0x3c011001, 0xc4200000, 0x44080000])
+      ]
+  , testGroup
+      "Move Float"
+      [ testCase "Can move pi from $f0 to $f1" $
+        assertEqual
+          ""
+          pi
+          (floatAt 1 $ runSeg [0x40490fdb] [0x3c011001, 0xc4200000, 0x46000046])
+      ]
+  , testGroup
+      "Move Double"
+      [ testCase "Can move pi from $f0 to $f2" $
+        assertEqual
+          ""
+          pi
+          (doubleAt 2 $
+           runSeg [0x54442d18, 0x400921fb] [0x3c011001, 0xd4200000, 0x46200086])
       ]
   ]
