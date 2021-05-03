5fead0a73a13bbc1a21656b86899d52fecf090c9
Matheus Fernandes <mcfernandes1110@gmail.com>
2021-05-03 15:04:46 -0300

    add configurable main

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,20 @@ import           System.Environment
 
 main :: IO ()
 main = do
-  args <- take 2 <$> getArgs
-  case args of
-    ["decode", fileName] -> decode fileName
-    ["run", fileName]    -> run 0 fileName
-    ["trace", fileName]  -> runTrace 0 fileName
-    _                    -> usage
+  (cmd, cfg, fileName) <- parseArgs . take 3 <$> getArgs
+  case cmd of
+    "decode" -> decode fileName
+    "run"    -> run cfg fileName
+    "trace"  -> runTrace cfg fileName
+    _        -> usage
 
 usage :: IO ()
 usage = do
   putStrLn "Usage:"
   putStrLn "\tminips decode program_name"
   putStrLn "\tminips run program_name"
+  putStrLn "\tminips trace program_name"
+
+parseArgs :: [String] -> (String, Int, String)
+parseArgs [cmd, fileName]         = (cmd, 0, fileName)
+parseArgs [cmd, config, fileName] = (cmd, read config - 1, fileName)
--- src/Lib/Memory/Configs.hs
+++ src/Lib/Memory/Configs.hs
@@ -4,7 +4,15 @@ import           Lib.Computer.Types
 import           Optics             (over, set, (.~))
 
 configs :: [Memory]
-configs = mkConfig <$> [config3]
+configs =
+  mkConfig <$>
+  [ []
+  , [mkUnifiedCache L1 Random 1024 1 32]
+  , [mkSplitCache L1 Random 512 1 32]
+  , [mkSplitCache L1 LRU 512 1 32]
+  , [mkSplitCache L1 LRU 512 4 32]
+  , [mkSplitCache L1 LRU 512 4 64, mkUnifiedCache L2 LRU 2048 8 64]
+  ]
 
 config2 :: [Memory -> Memory]
 config2 = [mkUnifiedCache L1 Random 1024 1 32]
